5ea924b8a4b10444ad287aedf8912f148804c014
dwairi <dwairi.razan1995@gmail.com>
2017-12-08 18:48:56 +0200

    sd
    sad

---
+++ js/config_firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA04CeBHXMk9jfOtKF4r6h8_7slaJK8qqk",
+    authDomain: "system-atched.firebaseapp.com",
+    databaseURL: "https://system-atched.firebaseio.com",
+    projectId: "system-atched",
+    storageBucket: "system-atched.appspot.com",
+    messagingSenderId: "501334655686"
+  };
+  firebase.initializeApp(config);
